34169fe4899d68fc149f062c04392b3bad8534c0
Dario Malchiodi <malchiodi@di.unimi.it>
2014-10-19 11:38:09 +0200

    First import

---
+++ src/it/unimi/di/malchiodi/HelloWorld.java
@@ -0,0 +1,10 @@
+package it.unimi.di.malchiodi;
+
+/**
+ * Created by malchiodi on 19/10/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
